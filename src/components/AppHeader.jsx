f9f738f54a7c53ba987b452e71cbdadd5fbadb9d
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-11 14:00:52 +0100

    edit style

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom"
 
 export default function AppHeader() {
     return (
-        <header className="bg-light py-3 px-4 shadow">
+        <header className="bg-primary-subtle py-3 px-4 shadow">
             <nav>
-                <NavLink to="/" className="text-decoration-none">
+                <NavLink to="/" className="text-decoration-none text-dark">
                     <strong>Bool Movies</strong>
                 </NavLink>
             </nav>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 export default function Banner({ pageTitle, pageSubtitle, pageDescription, children }) {
     return (
-        <div className="banner bg-primary text-white py-4 mb-5">
+        <div className="banner bg-dark text-white py-4 mb-5">
             <div className="container-lg">
                 <h1>{pageTitle}</h1>
                 <h3 className="text-muted">{pageSubtitle}</h3>
