da801e857314b4eebda53c00faba2038c33db27b
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-16 12:44:08 +0100

    add ldrs library and loader style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,18 @@
+import 'ldrs/ring2'
+
 export default function Loader() {
     return (
         <>
-            <div>Loading...</div>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "calc(100vh - 56px - 264px" }}>
+                <l-ring-2
+                    size="40"
+                    stroke="5"
+                    stroke-length="0.25"
+                    bg-opacity="0.1"
+                    speed="0.8"
+                    color="black"
+                ></l-ring-2>
+            </div >
         </>
     )
 }
