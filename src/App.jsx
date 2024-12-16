47b824ae79b3beb338fccd2afff980573d9662ff
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-16 12:36:40 +0100

    add global context and home page loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import './App.css'
 import AppLayout from './layouts/AppLayout'
 import HomePage from './pages/HomePage'
 import SingleMovie from './pages/SingleMovie'
 import NotFound from './pages/NotFound'
+import GlobalContext from './context/GlobalContext'
 
 function App() {
 
+  const [loader, setLoader] = useState(false)
+
+  const values = {
+    loader,
+    setLoader
+  }
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
+      <GlobalContext.Provider value={values}>
 
-          <Route element={<AppLayout />}>
-            <Route index element={<HomePage />}></Route>
-            <Route path='/movie/:id' element={<SingleMovie />}></Route>
-            <Route path='*' element={<NotFound />}></Route>
-          </Route>
+        <BrowserRouter>
+          <Routes>
 
-        </Routes>
-      </BrowserRouter>
+            <Route element={<AppLayout />}>
+              <Route index element={<HomePage />}></Route>
+              <Route path='/movie/:id' element={<SingleMovie />}></Route>
+              <Route path='*' element={<NotFound />}></Route>
+            </Route>
+
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
+
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Banner from "../components/Banner";
 import MovieCard from "../components/MovieCard";
+import Loader from "../components/loader";
+import GlobalContext from "../context/GlobalContext";
 
 export default function HomePage() {
     // logic
     const [movies, setMovies] = useState([]);
+    const { loader, setLoader } = useContext(GlobalContext)
 
     const url = "http://localhost:3009/api/films";
 
@@ -14,6 +17,7 @@ export default function HomePage() {
             .then(response => response.json())
             .then(response => {
                 setMovies(response.films)
+                setLoader(false)
             })
             .catch(error => {
                 console.error('Error fetching data:', error)
@@ -22,6 +26,7 @@ export default function HomePage() {
 
     // fetch when component is uploaded
     useEffect(() => {
+        setLoader(true)
         fetchData(url)
     }, []);
 
@@ -30,22 +35,26 @@ export default function HomePage() {
     // render
     return (
         <>
-            <Banner pageTitle="Bool Movies" pageSubtitle="The greatest movies community" pageDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, autem hic. Aspernatur ratione officia voluptatibus esse repellat sunt vero. Est quam illum veritatis dolor dicta, ratione fugit sint adipisci non?" />
+            {loader ? <Loader /> : (
+                <>
+                    <Banner pageTitle="Bool Movies" pageSubtitle="The greatest movies community" pageDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, autem hic. Aspernatur ratione officia voluptatibus esse repellat sunt vero. Est quam illum veritatis dolor dicta, ratione fugit sint adipisci non?" />
 
-            <div className="container mb-5">
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+                    <div className="container mb-5">
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
 
-                    {movies ?
-                        movies.map(movie => (
-                            <div className="col" key={movie.id}>
-                                <MovieCard movie={movie} />
-                            </div>
-                        )) : (
-                            <h1>Something went wrong 😫</h1>
-                        )
-                    }
-                </div>
-            </div>
+                            {movies ?
+                                movies.map(movie => (
+                                    <div className="col" key={movie.id}>
+                                        <MovieCard movie={movie} />
+                                    </div>
+                                )) : (
+                                    <h1>Something went wrong 😫</h1>
+                                )
+                            }
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     )
 }
